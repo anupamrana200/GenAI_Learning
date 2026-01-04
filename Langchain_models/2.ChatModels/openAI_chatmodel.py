5ae10d24a394409a0e2445f0f7a2ea2664046e8e
Anupam Rana <141600262+anupamrana200@users.noreply.github.com>
2026-01-04 09:03:22 +0530

    feat: Add initial OpenAI ChatModel example and prompt environment setup.

--- Langchain_models/2.ChatModels/openAI_chatmodel.py
+++ Langchain_models/2.ChatModels/openAI_chatmodel.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-model = ChatOpenAI(model_name="gpt-4", temperature=0.9,max_completion_tokens=100)
+model = ChatOpenAI(model_name="gpt-4", temperature=0.9,max_completion_tokens=200)
 
 result = model.invoke("write a story about india?")
 print(result.content)
---
+++ Langchain_models/Langchain_Prompts/prompts_ui.py
@@ -0,0 +1,5 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()  # Load environment variables from .env file
+
